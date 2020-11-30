d4e14dcde0021347cbd6bd1e6ef6a189b952e840
Amelie <amelie.marduel@outlook.com>
2020-11-30 15:21:18 +0100

    30/11/2020

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -3,21 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\File;
 
 class UploadController extends AbstractController
 {
     /**
      * @Route("stock-images")
      */
-    public function stockImages(Request $request)    {
-        $data = $request->files->get('file');      //récupère dans la request le files de nom 'file'
 
-        $fileName = $data->getClientOriginalName();      //récupère et stock dans fileName, le nom du fichier
-        $data->move('public/images', $fileName); //choisir l'endroit d'ou est le fichier
+    public function storeImages(Request $request) {
+        $data = $request->files->get('file');    //récupère dans la request le files de nom 'file'
+
+        $fileName = $data->getClientOriginalName();        //récupère et stock dans fileName, le nom du fichier
+        
+        try {
+            $data->move($this->getParameter('stockImages'), $fileName); 
+        } catch (FileException $e) {
+            return new Response($e);
+        }         
 
         return new Response($fileName);
     }
 }
+
